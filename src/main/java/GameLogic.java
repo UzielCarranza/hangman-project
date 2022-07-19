0451bc85ad55b9095bd2fa518fa5577221f9a06b
UzielCarranza <uzielcarranzac97@gmail.com>
2022-07-19 13:50:24 -0400

    Added adjectives and pronouns for testing the game

--- src/main/java/GameLogic.java
+++ src/main/java/GameLogic.java
@@ -2,15 +2,15 @@ import java.util.Random;
 
 public class GameLogic {
     private final Random rand = new Random(); // call instance of Random object
-    private final String[] words = {"one", "cat", "fish", "car", "race"}; // array of words
-    private final String[] words2 = {"big", "small", "medium", "large", "giant"};
+    private final String[] adjectives = {"charming", "cruel", "fantastic", "gentle", "huge", "perfect", "rough", "sharp", "tasty"}; // array of words
+    private final String[] nouns = {"portugal", "potato", "queen", "rain", "rainbow", " banana", "balloon", "horse"};
 
     public GameLogic() {
     }
 
     public void printRandomWord() {
 
-        System.out.println("Word to be guessed: " + words2[rand.nextInt(words2.length)] + " " +  words[rand.nextInt(words.length)]);
+        System.out.println("Word to be guessed: " + adjectives[rand.nextInt(adjectives.length)] + " " + nouns[rand.nextInt(nouns.length)]);
     }
 
 }
