9fcb9cf0e90808e70cb622d1f6aed7492e24d080
UzielCarranza <uzielcarranzac97@gmail.com>
2022-07-19 13:26:40 -0400

    added main screen scenario

--- src/main/java/Scenarios.java
+++ src/main/java/Scenarios.java
@@ -3,6 +3,18 @@ public class Scenarios {
     public Scenarios() {
     }
 
+    public void mainScreen() {
+        //Ascii Art
+        System.out.println(" _                                             ");
+        System.out.println("| |                                            ");
+        System.out.println("| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  ");
+        System.out.println("| '_ \\ / _` | '_ \\ / _` | '_ ` _ \\ / _` | '_ \\");
+        System.out.println("| | | | (_| | | | | (_| | | | | | | (_| | | | | ");
+        System.out.println("|_| |_|\\__,_|_| |_|\\__, |_| |_| |_|\\__,_|_| |_|");
+        System.out.println("                    __/ |                      ");
+        System.out.println("                   |___/  \n");
+    }
+
     public void firstTry() {
         System.out.println("   |");
         System.out.println("   |");
