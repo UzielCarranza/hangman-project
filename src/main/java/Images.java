8c696afc29e1717a47cc6ffb47ac24e9ab6dbd7e
UzielCarranza <uzielcarranzac97@gmail.com>
2022-07-19 13:00:08 -0400

    Added images to the game

---
+++ src/main/java/Images.java
@@ -0,0 +1,100 @@
+public class Images {
+
+
+    public Images() {
+    }
+
+    public void firstTry() {
+        System.out.println("Wrong guess, try again");
+        System.out.println("   |");
+        System.out.println("   |");
+        System.out.println("   |");
+        System.out.println("   |");
+        System.out.println("   |");
+        System.out.println("   |");
+        System.out.println("   |");
+        System.out.println("___|___");
+    }
+
+    public void secondTry() {
+        System.out.println("Wrong guess, try again");
+        System.out.println("   ____________");
+        System.out.println("   |");
+        System.out.println("   |");
+        System.out.println("   |");
+        System.out.println("   |");
+        System.out.println("   |");
+        System.out.println("   |");
+        System.out.println("   | ");
+        System.out.println("___|___");
+    }
+
+    public void thirdTry() {
+        System.out.println("Wrong guess, try again");
+        System.out.println("   ____________");
+        System.out.println("   |");
+        System.out.println("   |");
+        System.out.println("   |");
+        System.out.println("   |");
+        System.out.println("   |");
+        System.out.println("   |");
+        System.out.println("   | ");
+        System.out.println("___|___");
+    }
+
+    public void fourthTry() {
+        System.out.println("Wrong guess, try again");
+        System.out.println("   ____________");
+        System.out.println("   |          _|_");
+        System.out.println("   |         /   \\");
+        System.out.println("   |        |     |");
+        System.out.println("   |         \\_ _/");
+        System.out.println("   |");
+        System.out.println("   |");
+        System.out.println("   |");
+        System.out.println("___|___");
+    }
+
+    public void fifthTry() {
+        System.out.println("Wrong guess, try again");
+        System.out.println("   ____________");
+        System.out.println("   |          _|_");
+        System.out.println("   |         /   \\");
+        System.out.println("   |        |     |");
+        System.out.println("   |         \\_ _/");
+        System.out.println("   |           |");
+        System.out.println("   |           |");
+        System.out.println("   |");
+        System.out.println("___|___");
+    }
+
+
+    public void sixthTry() {
+        System.out.println("Wrong guess, try again");
+        System.out.println("   ____________");
+        System.out.println("   |          _|_");
+        System.out.println("   |         /   \\");
+        System.out.println("   |        |     |");
+        System.out.println("   |         \\_ _/");
+        System.out.println("   |           |");
+        System.out.println("   |           |");
+        System.out.println("   |          / \\ ");
+        System.out.println("___|___      /   \\");
+    }
+
+
+    public void seventhTry() {
+        System.out.println("GAME OVER!");
+        System.out.println("   ____________");
+        System.out.println("   |          _|_");
+        System.out.println("   |         /   \\");
+        System.out.println("   |        |     |");
+        System.out.println("   |         \\_ _/");
+        System.out.println("   |          _|_");
+        System.out.println("   |         / | \\");
+        System.out.println("   |          / \\ ");
+        System.out.println("___|___      /   \\");
+        System.out.println("GAME OVER! The word was " + wordToGuessed);
+    }
+
+}
